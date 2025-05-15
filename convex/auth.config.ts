9a62e3ae26299aa8e00305acb791eaf892187d7c
vaibhav lokunde <vaibhav72004@gmail.com>
2025-05-15 08:29:29 +0530

    DONE

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://still-thrush-78.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ]
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
+import ConvexClerkProvider from "@/components/providers/ConvexClerkProvider";
+import Navbar  from "@/components/Navbar";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -24,12 +27,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ConvexClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <SignedIn>
+              <div className="min-h-screen">
+                <Navbar />
+                <main className="px-4 sm:px-6 lg:px-8">{children}</main>
+              </div>
+            </SignedIn>
+
+            <SignedOut>
+              <RedirectToSignIn />
+            </SignedOut>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ConvexClerkProvider>
   );
 }
